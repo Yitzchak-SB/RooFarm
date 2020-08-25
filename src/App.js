8659b5d86ae0d83776c76c5289782a497e3c5768
Dovid Baars <dovidbaars@gmail.com>
2020-08-25 17:11:37 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,40 +1,21 @@
-<<<<<<< HEAD
-import React, { Fragment } from 'react';
-import './App.css';
-import HowRoofFarmsWorks from './components/HowRoofFarmsWorks'
-import RoofFarmsBenefits from './components/RoofFarmsBenefits'
-import logo from "./logo.svg";
-
-
-function App() {
-  return (
-    <Fragment>
-    <div className="App">
-      <HowRoofFarmsWorks />
-      <RoofFarmsBenefits />
-    </div>
-    </Fragment>
-
-  );
-=======
 import React, { Fragment } from "react";
+import "./App.css";
+import HowRoofFarmsWorks from "./components/HowRoofFarmsWorks";
+import RoofFarmsBenefits from "./components/RoofFarmsBenefits";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import GetStartedButton from "./components/GetStartedButton";
 import AddressInput from "./components/AddressInput";
 
-function App() {
-<<<<<<< HEAD
-  return <Fragment></Fragment>;
->>>>>>> 8395d474d9aed79b7b3ffbf2a2e20e92ccd212e2
-=======
+const App = () => {
   return (
     <Fragment>
       <CssBaseline />
       <GetStartedButton />
       <AddressInput />
+      <HowRoofFarmsWorks />
+      <RoofFarmsBenefits />
     </Fragment>
   );
->>>>>>> 811786b3fc0d7b452d4793a729179b25fdb23be8
-}
+};
 
 export default App;
