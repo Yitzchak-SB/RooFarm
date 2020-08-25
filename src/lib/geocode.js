712d7343741edbe6d8fda8744740ce122207721c
Dovid Baars <dovidbaars@gmail.com>
2020-08-25 18:14:45 +0300

    backend route

--- src/lib/geocode.js
+++ src/lib/geocode.js
@@ -10,7 +10,7 @@ export const geocodeGenerator = (address) => {
       console.log(lat, lng);
       console.log(response.results[0].geometry.location);
       axios.post(
-        "http://127.0.0.1:5000/",
+        "hhttps://immense-lowlands-41652.herokuapp.com/",
         response.results[0].geometry.location
       );
     },
