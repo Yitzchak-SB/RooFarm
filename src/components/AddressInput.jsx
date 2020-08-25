77d85d9ed213c14d9049f3cc2555f5edb002c766
Dovid Baars <dovidbaars@gmail.com>
2020-08-25 22:42:04 +0300

    Revert "Merge branch 'backend'"
    This reverts commit bb9861299d2bb021ce0dfd5b933a4bcdaf1609d5.

--- src/components/AddressInput.jsx
+++ src/components/AddressInput.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
 import IconButton from "@material-ui/core/IconButton";
-import Autocomplete from "./Autocomplete";
-import FilterVintageTwoToneIcon from "@material-ui/icons/FilterVintageTwoTone";
 import { geocodeGenerator } from "../lib/geocode";
+
+import FilterVintageTwoToneIcon from "@material-ui/icons/FilterVintageTwoTone";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,15 +25,22 @@ const useStyles = makeStyles((theme) => ({
 
 const AddressInput = () => {
   const classes = useStyles();
+  const [value, setValue] = useState("");
 
-  const handleSubmit = async (address) => {
-    console.log(address);
-    geocodeGenerator(address);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    geocodeGenerator(value);
   };
 
   return (
     <Paper component="form" className={classes.root} onSubmit={handleSubmit}>
-      <Autocomplete submit={handleSubmit} />
+      <InputBase
+        className={classes.input}
+        placeholder="Enter Your Address To Get Started"
+        inputProps={{ "aria-label": "Enter Your Address To Get Started" }}
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      />
       <IconButton
         type="submit"
         className={classes.iconButton}
