2bdb1cf009b7306f5e3ae93d91de33ad57322595
nico <yasmin@hotmail.com>
2020-08-26 16:07:41 +0300

    mapp

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -10,45 +10,46 @@ const Map = () => {
   const lng = roofs.AddLng;
   const sqMtr = roofs.sqrd_meters;
   return (
+    <div>
     <Grid container id='grid-map'>   
-      <Grid item lg={6}>
-    <LeafletMap
-      id='map'
-      center={[lat[200], lng[200]]}
-      zoom={15}
-      attributionControl={true}
-      zoomControl={true}
-      doubleClickZoom={true}
-      scrollWheelZoom={true}
-      dragging={true}
-      animate={true}
-      easeLinearity={0.35}
-    >
-      <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+      <Grid item lg={6} >
+          <LeafletMap
+            id='map'
+            center={[lat[200], lng[200]]}
+            zoom={15}
+            attributionControl={true}
+            zoomControl={true}
+            doubleClickZoom={true}
+            scrollWheelZoom={true}
+            dragging={true}
+            animate={true}
+            easeLinearity={0.35}
+          >
+          <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
 
-      {Object.keys(addressDict).map(function (key, i) {
-        if (i % 10 === 0) {
-          return (
-            <Marker key={key} position={[lat[key], lng[key]]}>
-              <Popup>
-                This roof is perfect for roofarm! it has{" "}
-                {Math.floor(sqMtr[key])} square meters of usable space.
-              </Popup>
-            </Marker>
-          );
-        }
-      })}
-    </LeafletMap>
-    </Grid>
-    <Grid item lg={3}>
+          {Object.keys(addressDict).map(function (key, i) {
+              if (i % 10 === 0) {
+                return (
+                  <Marker key={key} position={[lat[key], lng[key]]}>
+                  <Popup>
+                    This roof is perfect for roofarm! it has{" "}
+                    {Math.floor(sqMtr[key])} square meters of usable space.
+                  </Popup>
+                  </Marker>
+                );
+              }
+            })}
+          </LeafletMap>
+      </Grid>
+      <Grid item lg={3}>
         <h2 id='text1'>The map on the left indicates some of the buldings in Paris where it can be installed a roofarm</h2>
         <h2 id='text2'>According to a survey made by Le Monde on 2019, less than 5% of the buldings implement it</h2>
-    </Grid>
-    <Grid item lg={3}>
+      </Grid>
+      <Grid item lg={3}>
         <h2 id='text3'>More than 2 million people lives in Paris according to the last census and the city counts with over 15 thousands buildings</h2>
       </Grid>  
     </Grid>
-
+    </div>
   );
 };
 
