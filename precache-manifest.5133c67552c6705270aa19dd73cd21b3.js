dd0c2affcb8f4062ab5b1cf513466a0589c8c559
izgong <36205068+izgong@users.noreply.github.com>
2020-08-30 10:57:14 +0300

    Updates

---
+++ precache-manifest.5133c67552c6705270aa19dd73cd21b3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9230c35db0f764c723cc129c4823bc21",
+    "url": "/RooFarm/index.html"
+  },
+  {
+    "revision": "021f7a4a029b60abc40e",
+    "url": "/RooFarm/static/css/main.34887d37.chunk.css"
+  },
+  {
+    "revision": "8b03d2d2c891ceeb8542",
+    "url": "/RooFarm/static/js/2.15599ceb.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "/RooFarm/static/js/2.15599ceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "021f7a4a029b60abc40e",
+    "url": "/RooFarm/static/js/main.ee0e0f21.chunk.js"
+  },
+  {
+    "revision": "fd69c31fe49ec160b288",
+    "url": "/RooFarm/static/js/runtime-main.c5da9e82.js"
+  },
+  {
+    "revision": "f9baef8ac0d836e6486419e282e42336",
+    "url": "/RooFarm/static/media/Quicksand-VariableFont_wght.f9baef8a.ttf"
+  },
+  {
+    "revision": "034f80d12cf88b01f1e666a56b4107e1",
+    "url": "/RooFarm/static/media/cosecha.034f80d1.jpeg"
+  },
+  {
+    "revision": "02a3c7abe6ac194c2375ecc7b8f43e0f",
+    "url": "/RooFarm/static/media/rooffarm.02a3c7ab.jpeg"
+  }
+]);
