e41d671b05bf0bc0b2571472fcc12cf27fdc85f5
izgong <36205068+izgong@users.noreply.github.com>
2020-08-30 11:49:26 +0300

    Updates

---
+++ precache-manifest.6968818f8257da082b43a7a9046c7fc1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ecc91c2bc34a55d1cf91ab9d90d8b4",
+    "url": "/RooFarm/index.html"
+  },
+  {
+    "revision": "0ac11defbea954083685",
+    "url": "/RooFarm/static/css/main.bd580e8c.chunk.css"
+  },
+  {
+    "revision": "e4024108531f72c736b7",
+    "url": "/RooFarm/static/js/2.9cc44ccb.chunk.js"
+  },
+  {
+    "revision": "bec7469d29a482d28fb28dd50b1fa94d",
+    "url": "/RooFarm/static/js/2.9cc44ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac11defbea954083685",
+    "url": "/RooFarm/static/js/main.58667258.chunk.js"
+  },
+  {
+    "revision": "7807588e417432050b93",
+    "url": "/RooFarm/static/js/runtime-main.c2205214.js"
+  },
+  {
+    "revision": "1fbdbd1e081c8f1e3a300371950c9b3c",
+    "url": "/RooFarm/static/media/Dovid_Baars.1fbdbd1e.png"
+  },
+  {
+    "revision": "923fedcbc23f485985013bc33e9f070a",
+    "url": "/RooFarm/static/media/Nicolás_Videla_Guahnon.923fedcb.png"
+  },
+  {
+    "revision": "f9baef8ac0d836e6486419e282e42336",
+    "url": "/RooFarm/static/media/Quicksand-VariableFont_wght.f9baef8a.ttf"
+  },
+  {
+    "revision": "b331ada616a3f9009c5276392e36d2d0",
+    "url": "/RooFarm/static/media/Ron_Zehavi.b331ada6.png"
+  },
+  {
+    "revision": "dc60650e1f31e35d8ee7f27fcde43991",
+    "url": "/RooFarm/static/media/Sebastian_Kleiner.dc60650e.png"
+  },
+  {
+    "revision": "975e5d4d777c54d23e258a68e9d6ca71",
+    "url": "/RooFarm/static/media/Serah_Tapia.975e5d4d.png"
+  },
+  {
+    "revision": "4c4bdc97c60160703f660be9e69025b9",
+    "url": "/RooFarm/static/media/Solomiia_Ivychuk.4c4bdc97.png"
+  },
+  {
+    "revision": "1c36007382fc94d2c79d92a41cab916e",
+    "url": "/RooFarm/static/media/Yitzchak_Siegel-Bareket.1c360073.png"
+  },
+  {
+    "revision": "034f80d12cf88b01f1e666a56b4107e1",
+    "url": "/RooFarm/static/media/cosecha.034f80d1.jpeg"
+  },
+  {
+    "revision": "02a3c7abe6ac194c2375ecc7b8f43e0f",
+    "url": "/RooFarm/static/media/rooffarm.02a3c7ab.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RooFarm/precache-manifest.850fef0906c3abfca3e4292c86d174c4.js"
+  "/RooFarm/precache-manifest.6968818f8257da082b43a7a9046c7fc1.js"
 );
 
 self.addEventListener('message', (event) => {
